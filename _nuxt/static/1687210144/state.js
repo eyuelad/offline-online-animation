498bac1a94d0b54fd580672096b4b47f7ab5d860
eyuelad <37635654+eyuelad@users.noreply.github.com>
2023-06-19 21:33:21 +0000

    Build: (6340090)

--- _nuxt/static/1687210144/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foffline-online-animation\u002F_nuxt\u002Fstatic\u002F1687210144",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foffline-online-animation\u002F",assetsPath:"\u002Foffline-online-animation\u002F_nuxt\u002F",cdnURL:a}}}}(null));
